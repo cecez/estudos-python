fb67041433344d85424643787a719054a09177c0
Cezar Rosa <cecez86@gmail.com>
2021-12-31 10:04:22 -0300

    refactor: gerando lista de letras acertadas dinamicamente

--- 002-alura-python-3-parte-2-avançando-na-linguagem/aula-05/forca.py
+++ 002-alura-python-3-parte-2-avançando-na-linguagem/aula-05/forca.py
@@ -4,7 +4,7 @@ def jogar():
     print("*********************************")
 
     palavra_secreta = "cama".upper()
-    letras_acertadas = ["_", "_", "_", "_"]
+    letras_acertadas = ["_" for letra in palavra_secreta]
     letras_erradas = set()
 
     enforcou = False
