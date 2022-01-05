7ebcb9584e99ccdb2f78de724101cbaaa79206fd
Cezar Rosa <cecez86@gmail.com>
2022-01-05 10:25:33 -0300

    style: melhorando mensagens de vitória, derrota e durante o jogo

--- 002-alura-python-3-parte-2-avançando-na-linguagem/aula-07/forca.py
+++ 002-alura-python-3-parte-2-avançando-na-linguagem/aula-07/forca.py
@@ -4,13 +4,15 @@ import random
 def jogar():
     imprime_mensagem_abertura()
 
-    palavra_secreta = gera_palavra_secreta()
+    palavra_secreta = gera_palavra_secreta(nome_arquivo="palavras.txt", linha_inicial=0)  # parâmetro nomeado (pode
+    # ser usado em qualquer ordem)
     letras_acertadas = gera_lista_de_letras_acertadas(palavra_secreta)
     letras_erradas = set()
 
     enforcou = False
     acertou = False
     erros = 0
+    total_de_erros_para_enforcar = 7
 
     while not enforcou and not acertou:
         print(f"Palavra secreta: {letras_acertadas}")
@@ -27,13 +29,14 @@ def jogar():
         else:
             marca_chute_incorreto(letras_erradas, chute)
             erros = erros + 1
-            print(f"Letra errada! Restam {6 - erros} tentativas")
+            print(f"Letra errada! Restam {total_de_erros_para_enforcar - erros} tentativas")
+            desenha_forca(erros)
 
-        enforcou = erros == 6  # critério para enforcar, 6 erros
+        enforcou = erros == total_de_erros_para_enforcar  # critério para enforcar, 6 erros
         acertou = "_" not in letras_acertadas  # critério para vencer, nenhuma letra para descobrir
 
     if enforcou:
-        imprime_mensagem_derrota()
+        imprime_mensagem_derrota(palavra_secreta)
     elif acertou:
         imprime_mensagem_vitoria()
 
@@ -46,9 +49,9 @@ def imprime_mensagem_abertura():
     print("*********************************")
 
 
-def gera_palavra_secreta():
+def gera_palavra_secreta(linha_inicial=0, nome_arquivo="palavras.txt"):  # parâmetros opcionais e nomeados
     # gerando lista de palavras secretas
-    arquivo_de_palavras_secretas = open("palavras.txt", "r")
+    arquivo_de_palavras_secretas = open(nome_arquivo, "r")
     lista_de_palavras_secretas = []
     for possivel_palavra_secreta in arquivo_de_palavras_secretas:
         possivel_palavra_secreta = possivel_palavra_secreta.strip()  # remove caracteres de nova linha (\n)
@@ -56,7 +59,7 @@ def gera_palavra_secreta():
     arquivo_de_palavras_secretas.close()
 
     # escolhendo palavra aleatória
-    indice_aleatorio = random.randrange(0, len(lista_de_palavras_secretas))
+    indice_aleatorio = random.randrange(linha_inicial, len(lista_de_palavras_secretas))
     palavra_secreta = lista_de_palavras_secretas[indice_aleatorio]
     palavra_secreta = palavra_secreta.upper()
 
@@ -85,12 +88,90 @@ def marca_chute_incorreto(letras_erradas, chute):
     letras_erradas.add(chute)
 
 
-def imprime_mensagem_derrota():
-    print("- Que pena, você foi enforcado")
+def imprime_mensagem_derrota(palavra_secreta):
+    print("Puxa, você foi enforcado!")
+    print("A palavra era {}".format(palavra_secreta))
+    print("    _______________         ")
+    print("   /               \       ")
+    print("  /                 \      ")
+    print("//                   \/\  ")
+    print("\|   XXXX     XXXX   | /   ")
+    print(" |   XXXX     XXXX   |/     ")
+    print(" |   XXX       XXX   |      ")
+    print(" |                   |      ")
+    print(" \__      XXX      __/     ")
+    print("   |\     XXX     /|       ")
+    print("   | |           | |        ")
+    print("   | I I I I I I I |        ")
+    print("   |  I I I I I I  |        ")
+    print("   \_             _/       ")
+    print("     \_         _/         ")
+    print("       \_______/           ")
 
 
 def imprime_mensagem_vitoria():
-    print("--- PARABÉNS, você venceu! --- ")
+    print("Parabéns, você ganhou!")
+    print("       ___________      ")
+    print("      '._==_==_=_.'     ")
+    print("      .-\\:      /-.    ")
+    print("     | (|:.     |) |    ")
+    print("      '-|:.     |-'     ")
+    print("        \\::.    /      ")
+    print("         '::. .'        ")
+    print("           ) (          ")
+    print("         _.' '._        ")
+    print("        '-------'       ")
+
+
+def desenha_forca(erros):
+    print("  _______     ")
+    print(" |/      |    ")
+
+    if erros == 1:
+        print(" |      (_)   ")
+        print(" |            ")
+        print(" |            ")
+        print(" |            ")
+
+    if erros == 2:
+        print(" |      (_)   ")
+        print(" |      \     ")
+        print(" |            ")
+        print(" |            ")
+
+    if erros == 3:
+        print(" |      (_)   ")
+        print(" |      \|    ")
+        print(" |            ")
+        print(" |            ")
+
+    if erros == 4:
+        print(" |      (_)   ")
+        print(" |      \|/   ")
+        print(" |            ")
+        print(" |            ")
+
+    if erros == 5:
+        print(" |      (_)   ")
+        print(" |      \|/   ")
+        print(" |       |    ")
+        print(" |            ")
+
+    if erros == 6:
+        print(" |      (_)   ")
+        print(" |      \|/   ")
+        print(" |       |    ")
+        print(" |      /     ")
+
+    if erros == 7:
+        print(" |      (_)   ")
+        print(" |      \|/   ")
+        print(" |       |    ")
+        print(" |      / \   ")
+
+    print(" |            ")
+    print("_|___         ")
+    print()
 
 
 # indica que script foi chamado/executado diretamente pela linha de comando
