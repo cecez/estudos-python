be8d2bb0a0d0876e36425c2f7c3a0e6dbecb5a6c
Cezar Rosa <cecez86@gmail.com>
2022-01-07 14:31:56 -0300

    feat: concluindo inicializações, imprime tabuleiro, início do loop principal do jogo

--- 002-alura-python-3-parte-2-avançando-na-linguagem/projeto/jogo_da_velha.py
+++ 002-alura-python-3-parte-2-avançando-na-linguagem/projeto/jogo_da_velha.py
@@ -17,15 +17,42 @@ def obtem_jogadores():
     return {1: nome_jogador_1, 2: nome_jogador_2}
 
 
+def imprime_tabuleiro(tabuleiro):
+    print("    1   2   3 ")
+    print(" 1  {} | {} | {} ".format(tabuleiro[1][1], tabuleiro[1][2], tabuleiro[1][3]))
+    print("   -----------")
+    print(" 2  {} | {} | {} ".format(tabuleiro[2][1], tabuleiro[2][2], tabuleiro[2][3]))
+    print("   -----------")
+    print(" 3  {} | {} | {} ".format(tabuleiro[3][1], tabuleiro[3][2], tabuleiro[3][3]))
+
+
 def jogar():
+    # inicializações
     imprime_mensagem_inicial()
     jogadores = obtem_jogadores()
-    print(f'Que comecem os jogos! O duelo será {jogadores[1]} X {jogadores[2]}! Que tenham uma partida justa!')
+    jogador_da_vez = 1
     tabuleiro = inicializa_tabuleiro()
+    turno = 0
+    print(f'Que comecem os jogos! O duelo será {jogadores[1]} X {jogadores[2]}! Que tenham uma partida justa!')
 
-    tabuleiro[1][3] = 'X'
+    # loop principal do jogo
+    while True:
+        turno += 1
 
-    print(tabuleiro)
+        # condição temporária para sair do loop
+        if turno == 3:
+            break
+
+        imprime_tabuleiro(tabuleiro)
+        jogada = input("{}, qual sua jogada? (ex: 1-2, linha 1 - coluna 2)".format(jogadores[jogador_da_vez]))
+
+        # alterna jogador do próximo turno
+        if jogador_da_vez == 1:
+            jogador_da_vez = 2
+        else:
+            jogador_da_vez = 1
+
+    # término
 
 
 if __name__ == "__main__":
