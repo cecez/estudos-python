4bbe7ea40bbf28bd7a5922600d64c0f805e27ffb
Cezar Rosa <cecez86@gmail.com>
2022-01-10 11:06:14 -0300

    feat: jogo da velha concluído (testa jogada, marca jogada, verifica vencedor, verifica empate, exibe mensagem vencedor)

--- 002-alura-python-3-parte-2-avançando-na-linguagem/projeto/jogo_da_velha.py
+++ 002-alura-python-3-parte-2-avançando-na-linguagem/projeto/jogo_da_velha.py
@@ -14,7 +14,7 @@ def obtem_jogadores():
     # solicita nome dos jogadores
     nome_jogador_1 = input("Digite o nome do jogador 1 (símbolo 'o'):")
     nome_jogador_2 = input("Digite o nome do jogador 2 (símbolo 'x'):")
-    return {1: nome_jogador_1, 2: nome_jogador_2}
+    return {'o': nome_jogador_1, 'x': nome_jogador_2}
 
 
 def imprime_tabuleiro(tabuleiro):
@@ -26,33 +26,88 @@ def imprime_tabuleiro(tabuleiro):
     print(" 3  {} | {} | {} ".format(tabuleiro[3][1], tabuleiro[3][2], tabuleiro[3][3]))
 
 
+def alguem_ganhou(tabuleiro):
+    if (tabuleiro[1][1] == tabuleiro[1][2] == tabuleiro[1][3]) and (tabuleiro[1][1] != '_'):
+        return True
+    elif (tabuleiro[2][1] == tabuleiro[2][2] == tabuleiro[2][3]) and (tabuleiro[2][1] != '_'):
+        return True
+    elif (tabuleiro[3][1] == tabuleiro[3][2] == tabuleiro[3][3]) and (tabuleiro[3][1] != '_'):
+        return True
+    elif (tabuleiro[1][1] == tabuleiro[2][1] == tabuleiro[3][1]) and (tabuleiro[1][1] != '_'):
+        return True
+    elif (tabuleiro[1][2] == tabuleiro[2][2] == tabuleiro[3][2]) and (tabuleiro[1][2] != '_'):
+        return True
+    elif (tabuleiro[1][3] == tabuleiro[2][3] == tabuleiro[3][3]) and (tabuleiro[1][3] != '_'):
+        return True
+    elif (tabuleiro[1][1] == tabuleiro[2][2] == tabuleiro[3][3]) and (tabuleiro[1][1] != '_'):
+        return True
+    elif (tabuleiro[1][3] == tabuleiro[2][2] == tabuleiro[3][1]) and (tabuleiro[1][3] != '_'):
+        return True
+
+
+def marca_jogada(jogada, jogador_da_vez, tabuleiro):
+    linha = int(jogada[0])
+    coluna = int(jogada[1])
+    tabuleiro[linha][coluna] = jogador_da_vez
+
+
+def jogada_permitida(jogada, tabuleiro):
+    linha = int(jogada[0])
+    coluna = int(jogada[1])
+    return tabuleiro[linha][coluna] == '_'
+
+
 def jogar():
     # inicializações
     imprime_mensagem_inicial()
     jogadores = obtem_jogadores()
-    jogador_da_vez = 1
+    jogador_da_vez = 'o'
     tabuleiro = inicializa_tabuleiro()
-    turno = 0
-    print(f'Que comecem os jogos! O duelo será {jogadores[1]} X {jogadores[2]}! Que tenham uma partida justa!')
+    jogadas = 0
+    vencedor = False
+    print(f"Que comecem os jogos! O duelo será {jogadores['o']} X {jogadores['x']}! Que tenham uma partida justa!")
 
     # loop principal do jogo
     while True:
-        turno += 1
-
-        # condição temporária para sair do loop
-        if turno == 3:
-            break
 
         imprime_tabuleiro(tabuleiro)
-        jogada = input("{}, qual sua jogada? (ex: 1-2, linha 1 - coluna 2)".format(jogadores[jogador_da_vez]))
+
+        print("--- Sua vez {}".format(jogadores[jogador_da_vez]))
+        jogada = input("Qual a linha da sua jogada?")
+        jogada += input("Qual a coluna da sua jogada?")
+
+        if not jogada_permitida(jogada, tabuleiro):
+            print("Alguém já escolheu esta opção, tente novamente")
+            continue
+
+        marca_jogada(jogada, jogador_da_vez, tabuleiro)
+
+        jogadas += 1
+
+        # analisa critério de vitória
+        if alguem_ganhou(tabuleiro):
+            vencedor = True
+            break
+
+        # empate, não há mais jogadas disponíveis
+        if jogadas == 9:
+            break
 
         # alterna jogador do próximo turno
-        if jogador_da_vez == 1:
-            jogador_da_vez = 2
+        if jogador_da_vez == 'o':
+            jogador_da_vez = 'x'
         else:
-            jogador_da_vez = 1
+            jogador_da_vez = 'o'
 
     # término
+    print("----------------------------------")
+    print("-------- RESULTADO FINAL ---------")
+    print("----------------------------------")
+    if vencedor:
+        print("Temos um vencedor! Parabéns " + jogadores[jogador_da_vez] + "!")
+    else:
+        print(".... empate. nheh")
+    print("----------------------------------")
 
 
 if __name__ == "__main__":
--- 002-alura-python-3-parte-2-avançando-na-linguagem/projeto/readme.md
+++ 002-alura-python-3-parte-2-avançando-na-linguagem/projeto/readme.md
@@ -10,21 +10,23 @@ Fazer um jogo da velha simples em Python.
    - [x] Inicializa tabuleiro
   
 
-- [ ] Loop principal 
-  - [ ] encerra jogo se não há mais espaços
-  - [ ] encerra jogo se há vencedor
-  - [ ] Pede jogada do próximo jogador
+- [x] Loop principal 
+  - [x] encerra jogo se não há mais espaços
+  - [x] encerra jogo se há vencedor
+  - [x] Pede jogada do próximo jogador
 
 
-- [ ] Se terminou, exibe mensagem do vencedor
+- [x] Se terminou, exibe mensagem do vencedor
 
 ## Tarefas
 - [x] Estrutura para armazenar tabuleiro: Dicionário
 - [x] Função para imprimir tabuleiro (com coordenadas)
 
+```
     1   2   3
  1    |   |   
    -----------
  2    |   |   
    ------------
  3    |   |   
+```
